d717b6295b0b86d07c3679591017002fc6aa25bf
Felipe <felipe.pinto.2912@gmail.com>
2021-09-30 18:04:31 -0300

    created function to change square color on hover

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const container = document.querySelector('.container')
-for (x=1; x<=16*16; x++) {
-    const square = document.createElement('div');
-    let size = `${544/16 - 2}px`
-    square.className = "square";
-    // square.style.width = size
-    // square.style.height = size
+let size = 16
+container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+container.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
+
+for (x=1; x<=size*size; x++) {
+    const square = document.createElement('div');
+    square.className = "square";
+    square.setAttribute('onmouseover', 'paint()')
     container.appendChild(square)
+}
+
+const squares = document.querySelectorAll('.square')
+
+squares.forEach((square) => {
+    square.onmouseover = function() {paint(square)}  
+})
+
+function paint(square) {
+    square.style.backgroundColor = 'black'
 }
