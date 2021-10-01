33a45705ee2a36349fdf0e5ea142961b543af9ef
Felipe <felipe.pinto.2912@gmail.com>
2021-09-30 22:09:31 -0300

    added button to restart the grid

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
 const container = document.querySelector('.container')
 let size = 16
-container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', restart)
 
+function start() {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-for (x=1; x<=size*size; x++) {
-    const square = document.createElement('div');
-    square.className = "square";
-    square.setAttribute('onmouseover', 'paint()')
-    container.appendChild(square)
+    let x=1
+    while (x<=size*size) {
+        const square = document.createElement('div');
+        square.className = "square";   
+        container.appendChild(square)
+        x++
+    }
+
+    const squares = document.querySelectorAll('.square')
+    
+    squares.forEach((square) => {
+        square.onmouseover = function() {paint(square)}  
+    })
+    
+    function paint(square) {
+        square.style.backgroundColor = 'black'
+    }
 }
 
-const squares = document.querySelectorAll('.square')
+function restart() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
+    size = window.prompt('Select size.(SxS)', 'S=')
+    size = parseInt(size)
 
-squares.forEach((square) => {
-    square.onmouseover = function() {paint(square)}  
-})
 
-function paint(square) {
-    square.style.backgroundColor = 'black'
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    let x=1
+    while (x<=size*size) {
+        const square = document.createElement('div');
+        square.className = "square";   
+        container.appendChild(square)
+        x++
+    }
+    
+    const squares = document.querySelectorAll('.square')
+    
+    squares.forEach((square) => {
+        square.onmouseover = function() {paint(square)}  
+    })
+    
+    function paint(square) {
+        square.style.backgroundColor = 'black'
+    }
 }
+
+start()
